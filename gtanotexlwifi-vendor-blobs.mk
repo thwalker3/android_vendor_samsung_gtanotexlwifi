fda8db0d6daa82546639b710df561740b5de2012
Thomas Walker <thwalker3@gmail.com>
2019-02-21 20:35:21 -0500

    rebase to followmsi 32 bit

--- gtanotexlwifi-vendor-blobs.mk
+++ gtanotexlwifi-vendor-blobs.mk
@@ -41,8 +41,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtanotexlwifi/proprietary/app/mcRegistry/fffffffff0000000000000000000001b.tlbin:system/app/mcRegistry/fffffffff0000000000000000000001b.tlbin \
     vendor/samsung/gtanotexlwifi/proprietary/app/mcRegistry/fffffffff0000000000000000000001e.tlbin:system/app/mcRegistry/fffffffff0000000000000000000001e.tlbin \
     vendor/samsung/gtanotexlwifi/proprietary/bin/gpsd:system/bin/gpsd \
-    vendor/samsung/gtanotexlwifi/proprietary/bin/mcDriverDaemon:system/bin/mcDriverDaemon \
-    vendor/samsung/gtanotexlwifi/proprietary/bin/wcnss_filter:system/bin/wcnss_filter \
     vendor/samsung/gtanotexlwifi/proprietary/etc/bluetooth/av_performance.conf:system/etc/bluetooth/av_performance.conf \
     vendor/samsung/gtanotexlwifi/proprietary/etc/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
     vendor/samsung/gtanotexlwifi/proprietary/etc/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
@@ -51,21 +49,21 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/bdwlan30.bin:system/etc/firmware/bdwlan30.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/bdwlan32.bin:system/etc/firmware/bdwlan32.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/Data.msc:system/etc/firmware/Data.msc \
+    vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/nvm_tlv.bin:system/etc/firmware/nvm_tlv.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/nvm_tlv_1.3.bin:system/etc/firmware/nvm_tlv_1.3.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/nvm_tlv_2.1.bin:system/etc/firmware/nvm_tlv_2.1.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/nvm_tlv_3.0.bin:system/etc/firmware/nvm_tlv_3.0.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/nvm_tlv_3.2.bin:system/etc/firmware/nvm_tlv_3.2.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/nvm_tlv_tf_1.1.bin:system/etc/firmware/nvm_tlv_tf_1.1.bin \
-    vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/nvm_tlv.bin:system/etc/firmware/nvm_tlv.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/otp30.bin:system/etc/firmware/otp30.bin \
-    vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/qwlan30_ibss.bin:system/etc/firmware/qwlan30_ibss.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/qwlan30.bin:system/etc/firmware/qwlan30.bin \
+    vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/qwlan30_ibss.bin:system/etc/firmware/qwlan30_ibss.bin \
+    vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/rampatch_tlv.img:system/etc/firmware/rampatch_tlv.img \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/rampatch_tlv_1.3.tlv:system/etc/firmware/rampatch_tlv_1.3.tlv \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/rampatch_tlv_2.1.tlv:system/etc/firmware/rampatch_tlv_2.1.tlv \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/rampatch_tlv_3.0.tlv:system/etc/firmware/rampatch_tlv_3.0.tlv \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/rampatch_tlv_3.2.tlv:system/etc/firmware/rampatch_tlv_3.2.tlv \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/rampatch_tlv_tf_1.1.tlv:system/etc/firmware/rampatch_tlv_tf_1.1.tlv \
-    vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/rampatch_tlv.img:system/etc/firmware/rampatch_tlv.img \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/utf30.bin:system/etc/firmware/utf30.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/utfbd30.bin:system/etc/firmware/utfbd30.bin \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/utfbd32.bin:system/etc/firmware/utfbd32.bin \
@@ -73,10 +71,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/wlan/qcom_cfg.ini:system/etc/firmware/wlan/qcom_cfg.ini \
     vendor/samsung/gtanotexlwifi/proprietary/etc/firmware/wlan/WCNSS_cfg.dat:system/etc/firmware/wlan/WCNSS_cfg.dat \
     vendor/samsung/gtanotexlwifi/proprietary/etc/srm.bin:system/etc/srm.bin \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/hw/audio.primary.universal7870.so:system/lib/hw/audio.primary.universal7870.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/hw/camera.universal7870.so:system/lib/hw/camera.universal7870.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/hw/gralloc.exynos5.so:system/lib/hw/gralloc.exynos5.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/hw/sensors.universal7870.so:system/lib/hw/sensors.universal7870.so \
@@ -85,33 +80,28 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtanotexlwifi/proprietary/lib/lib_SoundAlive_SRC384_ver300.so:system/lib/lib_SoundAlive_SRC384_ver300.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/lib_soundaliveresampler.so:system/lib/lib_soundaliveresampler.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/lib_SoundBooster_ver700.so:system/lib/lib_SoundBooster_ver700.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libaudio-ril.so:system/lib/libaudio-ril.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libaudioroute.so:system/lib/libaudioroute.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libbt-vendor.so:system/lib/libbt-vendor.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libcamera_metadata.so:system/lib/libcamera_metadata.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libtonyalsa.so:system/lib/libtonyalsa.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libcodecdump.so:system/lib/libcodecdump.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libcsc.so:system/lib/libcsc.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libexynoscamera.so:system/lib/libexynoscamera.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libexynosgscaler.so:system/lib/libexynosgscaler.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libexynosscaler.so:system/lib/libexynosscaler.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libexynosv4l2.so:system/lib/libexynosv4l2.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libfloatingfeature.so:system/lib/libfloatingfeature.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libhwjpeg.so:system/lib/libhwjpeg.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libOpenCv.so:system/lib/libOpenCv.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libpreprocessing_nxp.so:system/lib/libpreprocessing_nxp.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libprotobuf-cpp-fooo.so:system/lib/libprotobuf-cpp-fooo.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libSamsungPostProcess.so:system/lib/libSamsungPostProcess.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libSamsungPostProcessConvertor.so:system/lib/libSamsungPostProcessConvertor.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libSamsungPreProcess.so:system/lib/libSamsungPreProcess.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libsamsungRecord_ns.so:system/lib/libsamsungRecord_ns.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libsamsungRecord.so:system/lib/libsamsungRecord.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libsamsungRecord_ns.so:system/lib/libsamsungRecord_ns.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libsamsungSoundbooster_plus.so:system/lib/libsamsungSoundbooster_plus.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libsecaudioinfo.so:system/lib/libsecaudioinfo.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libseccameracore.so:system/lib/libseccameracore.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libsensorlistener.so:system/lib/libsensorlistener.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libsensorservice.so:system/lib/libsensorservice.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libsomp.so:system/lib/libsomp.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libstainkiller.so:system/lib/libstainkiller.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libtinyalsa.so:system/lib/libtinyalsa.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libtinycompress.so:system/lib/libtinycompress.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/libuniplugin.so:system/lib/libuniplugin.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/modules/qca_cld/qca_cld_wlan.ko:system/lib/modules/qca_cld/qca_cld_wlan.ko \
     vendor/samsung/gtanotexlwifi/proprietary/lib/omx/libOMX.Exynos.AVC.Decoder.so:system/lib/omx/libOMX.Exynos.AVC.Decoder.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/omx/libOMX.Exynos.AVC.Encoder.so:system/lib/omx/libOMX.Exynos.AVC.Encoder.so \
     vendor/samsung/gtanotexlwifi/proprietary/lib/omx/libOMX.Exynos.HEVC.Decoder.so:system/lib/omx/libOMX.Exynos.HEVC.Decoder.so \
@@ -128,19 +118,21 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtanotexlwifi/proprietary/vendor/lib/egl/libGLES_mali.so:system/vendor/lib/egl/libGLES_mali.so \
     vendor/samsung/gtanotexlwifi/proprietary/vendor/lib/libLifevibes_lvverx.so:system/vendor/lib/libLifevibes_lvverx.so \
     vendor/samsung/gtanotexlwifi/proprietary/vendor/lib/libLifevibes_lvvetx.so:system/vendor/lib/libLifevibes_lvvetx.so \
-    vendor/samsung/gtanotexlwifi/proprietary/vendor/lib/libMcClient.so:system/vendor/lib/libMcClient.so \
-    vendor/samsung/gtanotexlwifi/proprietary/vendor/lib/libMcRegistry.so:system/vendor/lib/libMcRegistry.so \
-    vendor/samsung/gtanotexlwifi/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
+    vendor/samsung/gtanotexlwifi/proprietary/vendor/bin/wcnss_filter:system/vendor/bin/wcnss_filter
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/hw/camera.universal7870.so:system/lib/hw/camera.universal7870.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libexynoscamera.so:system/lib/libexynoscamera.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libcsc.so:system/lib/libcsc.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libexynosgscaler.so:system/lib/libexynosgscaler.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libexynosscaler.so:system/lib/libexynosscaler.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libexynosv4l2.so:system/lib/libexynosv4l2.so
+
+PRODUCT_COPY_FILES += \
     vendor/samsung/gtanotexlwifi/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
 
 PRODUCT_COPY_FILES += \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libaudio-ril.so:system/lib/libaudio-ril.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
-    vendor/samsung/gtanotexlwifi/proprietary/lib/librilutils.so:system/lib/librilutils.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libaptX_encoder.so:system/lib/libaptX_encoder.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libaptXHD_encoder.so:system/lib/libaptXHD_encoder.so \
+    vendor/samsung/gtanotexlwifi/proprietary/lib/libldacBT_enc.so:system/lib/libldacBT_enc.so
 
-PRODUCT_COPY_FILES += \
-    vendor/samsung/gtanotexlwifi/proprietary/bin/prepare_param.sh:system/bin/prepare_param.sh
-
-#    vendor/samsung/gtanotexlwifi/proprietary/lib/libril.so:system/lib/libril.so \
-#    vendor/samsung/gtanotexlwifi/proprietary/bin/rild:system/bin/rild \
